b212b128515ac7bdc27614f54cf595ce7f1a3eae
Tia Kou <tiadevops@gmail.com>
2020-12-14 23:12:27 +0000

    finished async logic

--- assault/http.py
+++ assault/http.py
@@ -1,16 +1,30 @@
 import asyncio
 import time
+import os
+import requests
+
 
 def fetch(url):
     """ Make the request and return the results """
-    pass
+    started_at = time.monotonic()
+    responce = requests.get(url)
+    request_time = time.monotonic() - started_at
+    return {"status_code": responce.status_code, "request_time": request_time}
 
-def worker(name, queue, results):
+async def worker(name, queue, results):
     """
     A function to take unmake requests from the queue and perform the work,
     then add results to the results list
     """
-    pass
+    loop = asyncio.get_event_loop()
+    while True:
+        url = await queue.get()
+        if os.getenv("DEBUG"):
+            print(f"{name} - Fetching {url}")
+        future_result = loop.run_in_executor(None, fetch, url)
+        result = await future_result
+        results.append(result)
+        queue.task_done()
 
 async def distribute_work(url, requests, concurrency, results):
     """ Divide up the work into batches and collect the final results
@@ -31,12 +45,17 @@ async def distribute_work(url, requests, concurrency, results):
     await queue.join()
     total_time = time.monotonic() - started_at
 
+    for task in tasks:
+        task.cancel()
+
+    print("---")
+    print(f"{concurrency} workers took {total_time:.2f} seconds to complete {len(results)} requests")
+
 def assault(url, requests, concurrency):
     """ Entry point to making requests """
     results = []
     asyncio.run(distribute_work(url, requests, concurrency, results))
     print(results)
-    pass
 
 
 
